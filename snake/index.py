d1e07538027bbbbd3619478abc4ea871ee1cc50b
AcquahBaiden <jbaiden@live.com>
2023-09-11 08:05:08 -0400

    here we go

--- snake/index.py
+++ snake/index.py
@@ -1,6 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
-from snake import Snake 
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +12,8 @@ screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -17,11 +21,29 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
 
-gamee_is_on = True
-while gamee_is_on:
+game_is_on = True
+while game_is_on:
     screen.update()
     time.sleep(0.1)
 
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.add_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    # if head collides with any segment in the tail:
 
 screen.exitonclick()
