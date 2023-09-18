61c7210eddbcd901866ef8f243e9257aaa1ed5fc
AcquahBaiden <jbaiden@live.com>
2023-09-18 14:33:18 -0400

    super class

--- pong/paddle.py
+++ pong/paddle.py
@@ -1,28 +1,27 @@
 from turtle import Screen, Turtle
 
-class Paddle:
+class Paddle(Turtle):
     def __init__(self, x_cor, y_cor):
+        super().__init__()
         self.x_cor = x_cor
         self.y_cor = y_cor
-        self.paddle = None
         self.create_paddle()
 
     def create_paddle(self):
-        self.paddle = Turtle()
-        self.paddle.shape("square")
-        self.paddle.color("white")
-        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
-        self.paddle.penup()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
         self.go_to_start()
 
     def go_to_start(self):
-        self.paddle.goto(self.x_cor, self.y_cor)
+        self.goto(self.x_cor, self.y_cor)
 
     def go_up(self):
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
 
